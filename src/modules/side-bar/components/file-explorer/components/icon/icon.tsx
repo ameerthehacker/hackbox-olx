96fbd38a71c8250787c9f8617ca67a0caaffbd4b
Ameer Jhan <ameerjhanprof@gmail.com>
2020-01-26 14:47:33 +0530

    fix: lint warnings

--- src/modules/side-bar/components/file-explorer/components/icon/icon.tsx
+++ src/modules/side-bar/components/file-explorer/components/icon/icon.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { ReactElement } from 'react';
 
 interface IconProps {
   icon: string;
 }
 
-export default function Icon({ icon }: IconProps) {
+export default function Icon({ icon }: IconProps): ReactElement {
   return <img alt="file-explorer icon" src={icon} />;
 }
