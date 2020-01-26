1fe72517197a5be1210c53195ade4ae211b953ac
Ameer Jhan <ameerjhanprof@gmail.com>
2020-01-26 16:15:51 +0530

    feat: add a iframe preview

--- src/modules/app/app.tsx
+++ src/modules/app/app.tsx
@@ -2,13 +2,17 @@ import React, { ReactElement } from 'react';
 import NavBar from '../../components/navbar/navbar';
 import SideBar from '../side-bar/side-bar';
 import Editor from '../editor/editor';
+import Preview from '../preview/preview';
 
 export default function App(): ReactElement {
   return (
     <>
       <NavBar />
       <SideBar>
-        <Editor language="javascript" value={`console.log('hello world');`} />
+        <>
+          <Editor language="javascript" value={`console.log('hello world');`} />
+          <Preview />
+        </>
       </SideBar>
     </>
   );
---
+++ src/modules/preview/preview.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Preview() {
+  return <iframe src="https://www.google.com" />;
+}
