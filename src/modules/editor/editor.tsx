f46f97b4907acf3582f88ad8c5169e81667280f5
Ameer Jhan <ameerjhanprof@gmail.com>
2020-01-26 20:40:01 +0530

    fix: add missing border to editor

--- src/modules/editor/editor.tsx
+++ src/modules/editor/editor.tsx
@@ -1,6 +1,6 @@
 import React, { ReactElement } from 'react';
 import MonacoEditor from '@monaco-editor/react';
-import { useColorMode } from '@chakra-ui/core';
+import { useColorMode, Box } from '@chakra-ui/core';
 import Loading from './components/loading/loading';
 
 interface EditorProps {
@@ -11,14 +11,16 @@ export default function Editor(props: EditorProps): ReactElement {
   const { colorMode } = useColorMode();
 
   return (
-    <MonacoEditor
-      loading={<Loading />}
-      {...props}
-      options={{
-        fontSize: 20
-      }}
-      height="calc(100vh - 55px)"
-      theme={colorMode === 'dark' ? 'vs-dark' : 'vs-light'}
-    />
+    <Box borderTopWidth="2px" width="100%">
+      <MonacoEditor
+        loading={<Loading />}
+        {...props}
+        options={{
+          fontSize: 20
+        }}
+        height="calc(100vh - 55px)"
+        theme={colorMode === 'dark' ? 'vs-dark' : 'vs-light'}
+      />
+    </Box>
   );
 }
