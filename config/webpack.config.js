7c475a4d367922f52f66f35adb0ec207391ec5eb
Ameer Jhan <ameerjhanprof@gmail.com>
2020-02-09 03:21:41 +0530

    chore: comment vendor for now

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -157,10 +157,10 @@ module.exports = function(webpackEnv) {
       // the line below with these two lines if you prefer the stock client:
       // require.resolve('webpack-dev-server/client') + '?/',
       // require.resolve('webpack/hot/dev-server'),
-      vendor: [
-        isEnvDevelopment &&
-          require.resolve('react-dev-utils/webpackHotDevClient')
-      ].filter(Boolean),
+      // vendor: [
+      //   isEnvDevelopment &&
+      //     require.resolve('react-dev-utils/webpackHotDevClient')
+      // ].filter(Boolean),
       // Finally, this is your app's code:
       main: paths.appIndexJs,
       preview: paths.previewIndexJs
